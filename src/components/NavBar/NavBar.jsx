3e7bfb81dc69b92fb9e6322c7cbc0bf50e13ce0b
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-03 18:14:17 +0530

    adding Navabar elements

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,20 @@ export default function NavBar() {
         <div className="main-logo">
             <h3>World Affairs</h3>
         </div>
+        <div className="navBar-links">
+            <p>Home</p>
+            <p>Top Headlines</p>
+            <p>India</p>
+            <p>World</p>
+            <select >
+              <option disabled={true}>Select category</option>
+              <option>Entertainment</option>
+              <option>Health</option>
+              <option>Science</option>
+              <option>Sports</option>
+              <option>Technology</option>
+            </select>
+        </div>
     </div>
   )
 }
