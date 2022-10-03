9e45b4bb33de4ef26c986de4139080d394c3ddaa
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-04 00:02:57 +0530

    adding icons to navbar items

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './NavBar.css'
+import indiaLogo from '../../assets/image/india.webp'
+import worldLogo from '../../assets/image/world.webp'
+import homeImage from '../../assets/image/house.webp'
 
 export default function NavBar() {
   return (
@@ -8,18 +11,22 @@ export default function NavBar() {
             <h3>World Affairs</h3>
         </div>
         <div className="navBar-links">
-            <p>Home</p>
-            <p>Top Headlines</p>
-            <p>India</p>
-            <p>World</p>
-            <select >
+            <p><img src={homeImage} alt="home logo" />Home</p>
+            <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+              </svg>
+              Top Headlines
+            </p>
+            <p><img src={indiaLogo} alt="india logo" />India</p>
+            <p><img src={worldLogo} alt="world logo" />World</p>
+            {/* <select >
               <option disabled={true}>Select category</option>
               <option>Entertainment</option>
               <option>Health</option>
               <option>Science</option>
               <option>Sports</option>
               <option>Technology</option>
-            </select>
+            </select> */}
         </div>
     </div>
   )
