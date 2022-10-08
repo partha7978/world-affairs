d688dbf69ffe5f475105216a2e2614afecf31ce7
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-09 00:16:06 +0530

    adding hamburger menu in small screen and changing navbar color in small screen

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -9,6 +9,11 @@ export default function News() {
           <div className="main-logo">
               <h3>World Affairs</h3>
           </div>
+          <p class="hamburger">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </p>
         </div>
         <div className="news-main-container">
           <div className="news-section">
