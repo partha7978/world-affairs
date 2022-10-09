bae97e39ca7f02032fab1425267087c600a55912
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-10 00:02:34 +0530

    adding a bottom hamburger menu

---
+++ src/components/Hamburger/Hamburger.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+
+export default function Hamburger() {
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+          event &&
+          event.type === 'keydown' &&
+          (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+    
+      const list = (anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        //   display="flex"
+        //   justifyContent="center"
+        //   alignItems="center"
+        //   flexDirection="column"
+        >
+          <List>
+            <ListItemButton>
+                <ListItemText primary={'Top Headlines'} />
+            </ListItemButton>
+          </List>
+          <Divider />
+          <List>
+            {['India', 'World', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
+
+  return (
+    <div>
+        <svg  onClick={toggleDrawer('bottom', true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
+
+        <React.Fragment key={'bottom'}>
+          <SwipeableDrawer
+            anchor={'bottom'}
+            open={state['bottom']}
+            onClose={toggleDrawer('bottom', false)}
+            onOpen={toggleDrawer('bottom', true)}
+          >
+            {list('bottom')}
+          </SwipeableDrawer>
+        </React.Fragment>
+
+
+
+    </div>
+  )
+}
