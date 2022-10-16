fb4ba07758cd7139966449baf67f7878a74b2a76
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-17 00:18:14 +0530

    changing the news component from function based to class based

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,38 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Hamburger from '../Hamburger/Hamburger'
 import NewsCard from '../NewsCard/NewsCard'
 import './News.css'
 
-export default function News() {
-  return (
-    <div className="news-container">
-        <div className="news-navBar">
-          <div className="main-logo">
-              <h3>World Affairs</h3>
-          </div>
-          <div className="hamburger">
-            <Hamburger />
-          </div>
-        </div>
-        <div className="news-main-container">
-          <div className="news-section">
-            <h2>Top Headlines - India</h2>
-            <div className="news-cards">
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
-              <NewsCard />
+export class News extends Component {
+  constructor() {
+    super();
+    console.log("Constructor from News Component");
+    //todo: State for the fetched news
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+  render() {
+    return (
+      <div className="news-container">
+          <div className="news-navBar">
+            <div className="main-logo">
+                <h3>World Affairs</h3>
+            </div>
+            <div className="hamburger">
+              <Hamburger />
             </div>
           </div>
-        </div>
-    </div>
-  )
+          <div className="news-main-container">
+            <div className="news-section">
+              <h2>Top Headlines - India</h2>
+              <div className="news-cards">
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+                <NewsCard />
+              </div>
+            </div>
+          </div>
+      </div>
+    )
+  }
+  
 }
+export default News;
