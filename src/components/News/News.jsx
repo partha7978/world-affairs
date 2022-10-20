fd2ed81d0e8087dae6b50606dc71e923c1b65ea6
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-21 00:06:50 +0530

    adding pagination button

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -40,7 +40,18 @@ export class News extends Component {
                   return <NewsCard key={element.url} title={element.title.split('-')[0]} description={element.description ? element.description : "No description found"} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} times={element.publishedAt}/>    
                 })}
               </div>
-              <button>Hello</button>
+              <div className="news-pagination">
+                <button className="news-pagination-btn">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                  </svg>
+                </button>
+                <button className="news-pagination-btn">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
       </div>
