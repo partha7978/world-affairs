2a567d8ef38ec689b6cefabd70380fbe867c32ad
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-10-27 16:15:28 +0530

    adding custom carousel arrow button

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -9,9 +9,47 @@ export default function Carousel() {
         "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
         "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     ];
+    const nav_btn_arrow_styles = {
+        prevArrow: (
+            <button className="arrow-btn">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                </svg>
+            </button>
+        ),
+        nextArrow: (
+            <button className="arrow-btn">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                    />
+                </svg>
+            </button>
+        ),
+    };
     return (
         <div>
-            <Fade>
+            <Fade {...nav_btn_arrow_styles}>
                 <div className="each-slide">
                     <div className="news-img">
                         <img alt="img" src={images[0]} />
@@ -22,10 +60,9 @@ export default function Carousel() {
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Quisquam, quae.
                         </p>
-
                     </div>
                 </div>
-               <div className="each-slide">
+                <div className="each-slide">
                     <div>
                         <img alt="img" src={images[1]} />
                     </div>
@@ -47,7 +84,6 @@ export default function Carousel() {
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Quisquam, quae.
                         </p>
-                        
                     </div>
                 </div>
             </Fade>
