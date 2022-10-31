84ef8587b8e440c33510353c160933e698f2fe83
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-11-01 00:02:18 +0530

    fixing minor sidebar styles and adding hyperlink to carousel read more button

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,11 +4,6 @@ import "react-slideshow-image/dist/styles.css";
 import "./Carousel.css";
 
 export default function Carousel({ articles }) {
-    const images = [
-        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-        "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    ];
     const nav_btn_arrow_styles = {
         prevArrow: (
             <button className="arrow-btn">
@@ -63,7 +58,7 @@ export default function Carousel({ articles }) {
                                         {element.title.split('-')[0]}
                                     </h4>
                                     <div className="btn-read">
-                                        <button>Read More</button>
+                                        <a href={element.url} target="_blank" rel="noreferrer"><button>Read More</button></a>
                                     </div>
                                 </div>
                             </div>
