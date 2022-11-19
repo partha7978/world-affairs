d7a460bceca8c452c6d53a4af2c80c51472909d5
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-11-19 16:22:21 +0530

    adding date and days chip on card

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -16,33 +16,42 @@ export default function NewsCard({
     source,
     times,
 }) {
+
+    //* function for showing date in proper format
+    const showDate = () => {
+        let Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let Months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+        let day = Days[new Date(times).getDay()];
+        let month = Months[new Date(times).getMonth()];
+        let date = new Date(times).getDate();
+        return `${day} - ${date} ${month}`;
+    };
     return (
         <Card className="newsCard">
-            {/* <CardActionArea>   */} {/* Commenting this because this is a clickable section and if inside this im adding any button its giving me "button cannot appear as a descendant of button" error. This is because parent(card content) is clickable and inside this the child(button) is also clickable  */}
-                <CardMedia
-                    // sx={{margin: 1.2, borderRadius: '10px', maxWidth: '93%'}}
-                    component="img"
-                    height="140"
-                    image={imageUrl}
-                    alt="No image found"
-                />
-                <CardContent sx={{ paddingBottom: 1.2 }}>
-                    <div>
-                    <button className="card-chip">{source}</button>
-                    </div>
-                    
-                    <h5 className="title">{title}</h5>
-                    <Typography variant="body2" color="text.secondary">
-                        {description}
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        sx={{ marginTop: "0.5rem", fontSize: "0.7rem" }}
-                    >
-                    {new Date(times).toGMTString()}
-                    </Typography>
-                </CardContent>
+            {/* <CardActionArea>   */}
+            {/* Commenting this because this is a clickable section and 
+            if inside this im adding any button its giving me "button 
+            cannot appear as a descendant of button" error. This is because 
+            parent(card content) is clickable and inside this the child(button) is also clickable  
+            Search in google for more info. */}
+            <CardMedia
+                // sx={{margin: 1.2, borderRadius: '10px', maxWidth: '93%'}}
+                component="img"
+                height="140"
+                image={imageUrl}
+                alt="No image found"
+            />
+            <CardContent sx={{ paddingBottom: 1.2 }}>
+                <div className="card-header">
+                    <button className="card-chip-primary">{source}</button>
+                    <button className="card-chip-secondary">{showDate()}</button>
+                </div>
+
+                <h5 className="title">{title}</h5>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
             {/* </CardActionArea> */}
             <CardActions sx={{ paddingTop: 0 }}>
                 <a
