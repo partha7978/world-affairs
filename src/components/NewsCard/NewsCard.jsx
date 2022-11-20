efd9a95c6e0f5ab9894bc36a2802f4165de59e90
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-11-20 17:44:04 +0530

    changing the card style and adding a new btn

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -4,7 +4,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActions } from "@mui/material";
+// import { Button, CardActions } from "@mui/material";
 
 export default function NewsCard({
     title,
@@ -53,37 +53,22 @@ export default function NewsCard({
                 </Typography>
             </CardContent>
             {/* </CardActionArea> */}
-            <CardActions sx={{ paddingTop: 0 }}>
+            <CardContent sx={{ paddingTop: 0 }}>
                 <a
                     href={newsUrl}
                     target="_blank"
                     rel="noreferrer"
                     className="news-link"
                 >
-                    <Button
-                        size="small"
-                        color="primary"
-                        sx={{ paddingLeft: "8px" }}
-                    >
+                    <button className="read-more-btn">
                         Read more
-                    </Button>
-                </a>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
 
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 shareIcon"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
-                    />
-                </svg>
-            </CardActions>
+                    </button>
+                </a>
+            </CardContent>
         </Card>
     );
 }
