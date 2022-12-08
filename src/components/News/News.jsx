715d17a5dbff1c8d146524a44e784cd4cf46b2ee
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2022-12-09 00:08:12 +0530

    fixing pagination button and change it from <button> to <p>

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -235,7 +235,7 @@ export class News extends Component {
                             )}
                             {!this.state.loading && 
                                 <div className="news-pagination">
-                                <button
+                                <p
                                     className="news-pagination-btn"
                                     onClick={this.handlePreviousPage}
                                 >
@@ -253,11 +253,11 @@ export class News extends Component {
                                             d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
                                         />
                                     </svg>
-                                </button>
-                                <button className="page-show-btn">
+                                </p>
+                                <p className="page-show-btn">
                                     {this.state.page}
-                                </button>
-                                <button
+                                </p>
+                                <p
                                     className="news-pagination-btn"
                                     onClick={this.handleNextPage}
                                 >
@@ -275,7 +275,7 @@ export class News extends Component {
                                             d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
                                         />
                                     </svg>
-                                </button>
+                                </p>
                             </div>
                             }
                         </div>
