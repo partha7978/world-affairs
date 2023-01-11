d23d5dff626030e10b9590fa1d2249eda3d96fb0
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-01-11 17:00:48 +0530

    adding dark mode hover effect to sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -41,17 +41,17 @@ export default function NavBar({darkMode, setDarkMode}) {
             </button>
         </div>
         <div className="navBar-links">
-            <Link className="nav-link-item" to="/home" ><p style={{backgroundColor: darkMode && '#262626', color: darkMode && '#ffffff'}}><img src={homeImage} alt="home logo" />Home</p></Link>
+            <Link className="nav-link-item" to="/home" ><p id = {`${darkMode && 'dark-foreground'}`}><img src={homeImage} alt="home logo" />Home</p></Link>
             <Link className="nav-link-item" to="/" >
-                <p style={{backgroundColor: darkMode && '#262626', color: darkMode && '#ffffff'}}>
+                <p id = {`${darkMode && 'dark-foreground'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                     </svg>
                     Top Headlines
                 </p>
             </Link>
-            <Link className="nav-link-item" to="/in" ><p style={{backgroundColor: darkMode && '#262626', color: darkMode && '#ffffff'}}><img src={indiaLogo} alt="india logo" />India</p></Link>
-            <Link className="nav-link-item" to="/world" ><p style={{backgroundColor: darkMode && '#262626', color: darkMode && '#ffffff'}}><img src={worldLogo} alt="world logo" />World</p></Link>
+            <Link className="nav-link-item" to="/in" ><p id = {`${darkMode && 'dark-foreground'}`}><img src={indiaLogo} alt="india logo" />India</p></Link>
+            <Link className="nav-link-item" to="/world" ><p id = {`${darkMode && 'dark-foreground'}`}><img src={worldLogo} alt="world logo" />World</p></Link>
             <div className="navBar-chip-countries">
                 <span>Filter by countries</span>
                 <Divider sx={{backgroundColor: '#ffffff'}}/>
